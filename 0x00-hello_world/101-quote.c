74556da99039d6df19c04f381ce172d36998e491
PulehMaggz <pulehmaggz@gmail.com>
2024-06-20 14:52:14 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error code)
+ */
+int main(void)
+{
+    /* Write the message to the standard error */
+    write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+    /* Return 1 to indicate error */
+    return (1);
+}
