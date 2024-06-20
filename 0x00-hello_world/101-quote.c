bbb720b8e1ac7709232c672d6f28d7c8b65629be
PulehMaggz <pulehmaggz@gmail.com>
2024-06-20 15:03:36 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,9 @@
 int main(void)
 {
     /* Write the message to the standard error */
-    write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(STDERR_FILENO,
+  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
     /* Return 1 to indicate error */
-    return (1);
+return (1);
 }
