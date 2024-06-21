6ebf4d76502885b82cd741ca6572bc36bf40224b
PulehMaggz <pulehmaggz@gmail.com>
2024-06-21 07:38:30 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,26 +9,21 @@
  */
 int main(void)
 {
-    int n;
-
-    srand(time(0));  /* Seed rand() with current time */
-
-    n = rand() - RAND_MAX / 2;  /* Assign a random number to n */
-
-    printf("%d is ", n);
-
-    if (n > 0)
-    {
-        printf("positive\n");
-    }
-    else if (n == 0)
-    {
-        printf("zero\n");
-    }
-    else
-    {
-        printf("negative\n");
-    }
-
-    return 0;
+int n;
+srand(time(0));  /* Seed rand() with current time */
+n = rand() - RAND_MAX / 2;  /* Assign a random number to n */
+printf("%d is ", n);
+if (n > 0)
+{
+printf("positive\n");
+}
+else if (n == 0)
+{
+printf("zero\n");
+}
+else
+{
+printf("negative\n");
+}
+return 0;
 }
