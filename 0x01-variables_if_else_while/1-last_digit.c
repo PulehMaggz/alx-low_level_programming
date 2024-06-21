b5b28fc268e68af9880ca5bc565d3f67e10d9586
PulehMaggz <pulehmaggz@gmail.com>
2024-06-21 14:48:00 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,24 +11,22 @@ int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-/* Calculate the last digit of n */
+/* Calculate the last digit */
 last_digit = n % 10;
 
-/* Print the appropriate message */
-printf("Last digit of %d is %d and is ", n, last_digit);
+/* Print the result */
 if (last_digit > 5)
 {
-printf("greater than 5\n");
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit == 0)
 {
-printf("0\n");
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else
 {
-printf("less than 6 and not 0\n");
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 
 return (0);
 }
-
