a98253db7a92164a60183b9a6c1ddc0d13333b45
PulehMaggz <pulehmaggz@gmail.com>
2024-06-26 12:42:49 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+char text[] = "_putchar";
+int i;
+for (i = 0; text[i] != '\0'; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+return 0;
+}
