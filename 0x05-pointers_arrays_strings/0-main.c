29557e2d7d9c0ebd42d7b6b578c66a72a70bf9ae
PulehMaggz <pulehmaggz@gmail.com>
2024-07-01 15:40:18 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - Test function for reset_to_98
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 int n;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * reset_to_98 - Updates the value of an integer pointed to by n to 98.
+ * @n: Pointer to the integer to be updated
+ */
+
 void reset_to_98(int *n);
 int _putchar(char c);
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
