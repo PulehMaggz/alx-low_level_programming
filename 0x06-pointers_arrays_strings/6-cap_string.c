4ab4066fe2901ecf9397f132251e22c216c20099
PulehMaggz <pulehmaggz@gmail.com>
2024-07-03 13:21:09 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to modify.
+ *
+ * Return: Pointer to the modified string @str.
+ */
+char *cap_string(char *str)
+{
+    char *ptr = str;
+    int capitalize = 1; /* Capitalize next character */
+
+    while (*ptr != '\0')
+    {
+        if (capitalize && *ptr >= 'a' && *ptr <= 'z')
+            *ptr -= 32; /* Convert to uppercase */
+
+        capitalize = 0;
+
+        if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' ||
+            *ptr == ',' || *ptr == ';' || *ptr == '.' ||
+            *ptr == '!' || *ptr == '?' || *ptr == '"' ||
+            *ptr == '(' || *ptr == ')' || *ptr == '{' || *ptr == '}')
+            capitalize = 1; /* Next character should be capitalized */
+
+        ptr++;
+    }
+
+    return (str);
+}
