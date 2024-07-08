33621e19d1fb7bc96e685570b055582d345a8473
PulehMaggz <pulehmaggz@gmail.com>
2024-07-08 14:36:53 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,21 +2,30 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals of a square matrix
- * @a: the square matrix
- * @size: size of the matrix
+ * print_diagsums - Prints the sum of the two diagonals of a square matrix.
+ * @a: Pointer to the beginning of the 2D square matrix (flattened as 1D array)
+ * @size: Size of the square matrix
  *
- * Description: This function prints the sum of the two diagonals of a
- * square matrix of integers.
+ * Description: Assumes a is a 1D array representing a square matrix.
+ * Prints the sum of the diagonals separated by a comma.
  */
 void print_diagsums(int *a, int size)
 {
 int i;
 int sum1 = 0, sum2 = 0;
+
+/* Calculate sum of primary diagonal (top-left to bottom-right) */
 for (i = 0; i < size; i++)
 {
 sum1 += *(a + i * size + i);
+}
+
+/* Calculate sum of secondary diagonal (top-right to bottom-left)*/
+for (i = 0; i < size; i++)
+{
 sum2 += *(a + i * size + (size - 1 - i));
 }
+
+/* Print the sums in the required format*/
 printf("%d, %d\n", sum1, sum2);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,4 +12,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
+int _putchar(char c);
+
 #endif /* MAIN_H */
