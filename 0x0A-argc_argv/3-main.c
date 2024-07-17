251876b2300d136a9b159511b232f6bbcd60993a
PulehMaggz <pulehmaggz@gmail.com>
2024-07-17 10:07:57 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-main.c
+++ 0x0A-argc_argv/3-main.c
@@ -1,47 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+/**
+* main - Tests the 3-mul program with various arguments
+*
+* Return: 0 on success
+*/
 int main(void)
 {
-char *args[] = {"./mul", "2", "3", NULL};
+char *args1[] = {"./mul", "2", "3", NULL};
+char *args2[] = {"./mul", "2", "-3", NULL};
+char *args3[] = {"./mul", "2", "0", NULL};
+char *args4[] = {"./mul", "245", "3245342", NULL};
+char *args5[] = {"./mul", NULL};
 extern char **environ;
 printf("Test 1: ./mul 2 3\nExpected output: 6\n");
 if (fork() == 0)
 {
-execve("./mul", args, environ);
+execve("./mul", args1, environ);
 }
 wait(NULL);
-args[1] = "2";
-args[2] = "-3";
 printf("\nTest 2: ./mul 2 -3\nExpected output: -6\n");
 if (fork() == 0)
 {
-execve("./mul", args, environ);
+execve("./mul", args2, environ);
 }
 wait(NULL);
-args[1] = "2";
-args[2] = "0";
 printf("\nTest 3: ./mul 2 0\nExpected output: 0\n");
 if (fork() == 0)
 {
-execve("./mul", args, environ);
+execve("./mul", args3, environ);
 }
 wait(NULL);
-args[1] = "245";
-args[2] = "3245342";
 printf("\nTest 4: ./mul 245 3245342\nExpected output: 795108790\n");
 if (fork() == 0)
 {
-execve("./mul", args, environ);
+execve("./mul", args4, environ);
 }
 wait(NULL);
-args[1] = NULL;
-args[2] = NULL;
 printf("\nTest 5: ./mul\nExpected output: Error\n");
 if (fork() == 0)
 {
-execve("./mul", args, environ);
+execve("./mul", args5, environ);
 }
 wait(NULL);
-return 0;
+return (0);
 }
