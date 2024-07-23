ef866f46f0a2393c6946ad5e9663ebf169918963
PulehMaggz <pulehmaggz@gmail.com>
2024-07-23 14:13:04 -0700

    print dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,8 @@ char *owner;
 /* Function prototype */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 #endif /* DOG_H */
