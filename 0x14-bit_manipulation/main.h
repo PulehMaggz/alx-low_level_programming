3eae69cd6c6cf146ba6175105a26a12cd84bd159
PulehMaggz <pulehmaggz@gmail.com>
2024-08-09 12:11:48 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Pointer to a string of 0 and 1 chars.
+ *
+ * Return: The converted number, or 0 if:
+ * - b is NULL
+ * - There is one or more chars in the string b that is not 0 or 1.
+ */
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
