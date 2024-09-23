8fe1f9a4ce14b2fdb361abb1d44b4aaa85b8950b
PulehMaggz <pulehmaggz@gmail.com>
2024-09-23 15:55:33 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/dynamic_funcs.c
@@ -0,0 +1,100 @@
+int _putchar(char c) {
+    // Implementation
+    return 0;
+}
+
+int _islower(int c) {
+    // Implementation
+    return 0;
+}
+
+int _isalpha(int c) {
+    // Implementation
+    return 0;
+}
+
+int _abs(int n) {
+    // Implementation
+    return n < 0 ? -n : n;
+}
+
+int _isupper(int c) {
+    // Implementation
+    return 0;
+}
+
+int _isdigit(int c) {
+    // Implementation
+    return 0;
+}
+
+int _strlen(char *s) {
+    int len = 0;
+    while (*s++) len++;
+    return len;
+}
+
+void _puts(char *s) {
+    // Implementation
+}
+
+char *_strcpy(char *dest, char *src) {
+    // Implementation
+    return dest;
+}
+
+int _atoi(char *s) {
+    // Implementation
+    return 0;
+}
+
+char *_strcat(char *dest, char *src) {
+    // Implementation
+    return dest;
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    // Implementation
+    return dest;
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    // Implementation
+    return dest;
+}
+
+int _strcmp(char *s1, char *s2) {
+    // Implementation
+    return 0;
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    // Implementation
+    return s;
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    // Implementation
+    return dest;
+}
+
+char *_strchr(char *s, char c) {
+    // Implementation
+    return s;
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    // Implementation
+    return 0;
+}
+
+char *_strpbrk(char *s, char *accept) {
+    // Implementation
+    return s;
+}
+
+char *_strstr(char *haystack, char *needle) {
+    // Implementation
+    return haystack;
+}
+
