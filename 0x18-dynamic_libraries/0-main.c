32411264f1dab957b346b1b93f94490892f1e586
PulehMaggz <pulehmaggz@gmail.com>
2024-09-23 16:12:38 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"  // Your function prototypes
 
-int main(void)
-{
-    printf("%d\n", _strlen("My Dyn Lib"));
-    return (EXIT_SUCCESS);
+int main() {
+    char str[] = "Hello World";
+    _puts(str);
+    return 0;
 }
 
--- 0x18-dynamic_libraries/dynamic_funcs.c
+++ 0x18-dynamic_libraries/dynamic_funcs.c
@@ -1,100 +1,126 @@
+#include "main.h"
+#include <stddef.h>  // For NULL
+#include <string.h>  // For strchr and other string functions
+
 int _putchar(char c) {
-    // Implementation
-    return 0;
+    return (int)c;  // Just return the character for now as a placeholder
 }
 
 int _islower(int c) {
-    // Implementation
-    return 0;
+    return (c >= 'a' && c <= 'z');  // Check if it's lowercase
 }
 
 int _isalpha(int c) {
-    // Implementation
-    return 0;
-}
-
-int _abs(int n) {
-    // Implementation
-    return n < 0 ? -n : n;
+    return (_islower(c) || (c >= 'A' && c <= 'Z'));  // Check if it's alphabetic
 }
 
 int _isupper(int c) {
-    // Implementation
-    return 0;
+    return (c >= 'A' && c <= 'Z');  // Check if it's uppercase
 }
 
 int _isdigit(int c) {
-    // Implementation
-    return 0;
-}
-
-int _strlen(char *s) {
-    int len = 0;
-    while (*s++) len++;
-    return len;
+    return (c >= '0' && c <= '9');  // Check if it's a digit
 }
 
 void _puts(char *s) {
-    // Implementation
+    while (*s) {
+        _putchar(*s++);
+    }
 }
 
 char *_strcpy(char *dest, char *src) {
-    // Implementation
-    return dest;
+    char *temp = dest;
+    while ((*dest++ = *src++));
+    return temp;
 }
 
 int _atoi(char *s) {
-    // Implementation
-    return 0;
+    int num = 0;
+    while (*s && _isdigit(*s)) {
+        num = num * 10 + (*s++ - '0');
+    }
+    return num;
 }
 
 char *_strcat(char *dest, char *src) {
-    // Implementation
-    return dest;
+    char *temp = dest;
+    while (*dest) dest++;
+    while ((*dest++ = *src++));
+    return temp;
 }
 
 char *_strncat(char *dest, char *src, int n) {
-    // Implementation
-    return dest;
+    char *temp = dest;
+    while (*dest) dest++;
+    while (n-- && (*dest++ = *src++));
+    *dest = '\0';
+    return temp;
 }
 
 char *_strncpy(char *dest, char *src, int n) {
-    // Implementation
-    return dest;
+    char *temp = dest;
+    while (n-- && (*dest++ = *src++));
+    return temp;
 }
 
 int _strcmp(char *s1, char *s2) {
-    // Implementation
-    return 0;
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
 char *_memset(char *s, char b, unsigned int n) {
-    // Implementation
-    return s;
+    char *temp = s;
+    while (n--) {
+        *s++ = b;
+    }
+    return temp;
 }
 
 char *_memcpy(char *dest, char *src, unsigned int n) {
-    // Implementation
-    return dest;
+    char *temp = dest;
+    while (n--) {
+        *dest++ = *src++;
+    }
+    return temp;
 }
 
 char *_strchr(char *s, char c) {
-    // Implementation
-    return s;
+    while (*s) {
+        if (*s == c) return s;
+        s++;
+    }
+    return (c == '\0') ? s : NULL;
 }
 
 unsigned int _strspn(char *s, char *accept) {
-    // Implementation
-    return 0;
+    unsigned int count = 0;
+    while (*s && strchr(accept, *s++)) count++;
+    return count;
 }
 
 char *_strpbrk(char *s, char *accept) {
-    // Implementation
-    return s;
+    while (*s) {
+        if (strchr(accept, *s)) return s;
+        s++;
+    }
+    return NULL;
 }
 
 char *_strstr(char *haystack, char *needle) {
-    // Implementation
-    return haystack;
+    char *h, *n;
+    while (*haystack) {
+        h = haystack;
+        n = needle;
+        while (*n && (*h == *n)) {
+            h++;
+            n++;
+        }
+        if (!*n) return haystack;
+        haystack++;
+    }
+    return NULL;
 }
 
